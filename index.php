8c54a642821c556965259ca8bd37c6e03cc07a34
William Rodil <william@masondigital.co.uk>
2017-01-12 11:42:18 +0000

    Jide

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum, mauris quis bibendum interdum, risus turpis imperdiet dolor, sollicitudin convallis sapien augue quis metus. Vestibulum erat leo, sagittis vitae auctor porta, fringilla vel erat. Vivamus eleifend, dolor in accumsan imperdiet, diam enim fermentum massa, ac congue orci mi et diam. Nunc non purus ultricies, fermentum sem in, vulputate ligula. Donec mollis est arcu, quis vulputate elit ornare in. Maecenas pharetra ex quis rhoncus commodo. Suspendisse aliquet a est a feugiat. Duis laoreet vestibulum sem. Donec lacinia sagittis odio nec rhoncus.
 </p>
 <p>Second Amended content from local pushed to Git</p>
+<h3>Jide</h3>
 </body>
 </html>
